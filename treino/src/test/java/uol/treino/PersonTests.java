958e9b8175c80a77ae8787db59d494ec4ded23f3
Rafael Nissi <rafael.nissi@usp.br>
2020-07-06 19:21:05 -0300

    Criação de novos testes de integração

--- treino/src/test/java/uol/treino/PersonTests.java
+++ treino/src/test/java/uol/treino/PersonTests.java
@@ -3,11 +3,11 @@ package uol.treino;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import uol.treino.person.objects.Person;
 
@@ -16,6 +16,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PersonTests {
 
     @LocalServerPort
@@ -26,28 +27,19 @@ public class PersonTests {
     HttpHeaders headers = new HttpHeaders();
 
     @Test
-    public void completeIT() throws Exception {
-
-        Person person = new Person();
-        person.setName("Teste");
-        person.setAge(17);
-
-        HttpEntity<Person> entity = new HttpEntity<>(person, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/persons"), HttpMethod.POST, entity, String.class);
+    public void returnCreatedIfCreatedPersonSuccessfullyIT() throws Exception {
+        ResponseEntity<String> response = createPerson("Teste", 17);
 
         JSONObject jsonObject = new JSONObject(response.getBody());
 
         assertTrue(response.getStatusCode() == HttpStatus.CREATED);
         assertEquals(jsonObject.get("name"), "Teste");
         assertEquals(jsonObject.get("age"), 17);
-
-        getPerson();
-
     }
 
-    public void getPerson() throws Exception {
+    @Test
+    public void returnOkWhenSuccessfullyFindingUserByIdIT() throws Exception {
+        createPerson("Teste", 18);
         HttpEntity<Person> entity = new HttpEntity<>(null , headers);
 
         ResponseEntity<String> response = restTemplate.exchange(
@@ -58,6 +50,59 @@ public class PersonTests {
         assertTrue(response.getStatusCode() == HttpStatus.OK);
         assertEquals(jsonObject.get("name"), "Teste");
         assertEquals(jsonObject.get("age"), 18);
+    }
+
+    @Test
+    public void returnOkWhenDeletedPersonSuccessfullyIT() throws Exception {
+        createPerson("TesteDeleted", 21);
+        HttpEntity<Person> entity = new HttpEntity<>(null , headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/persons/1"), HttpMethod.DELETE, entity, String.class);
+
+        assertTrue(response.getStatusCode() == HttpStatus.OK);
+    }
+
+    @Test
+    public void returnOkWhenPatchNewDataIT() throws Exception {
+        createPerson("Teste", 18);
+
+        Person person = new Person();
+        person.setName("TestePatch");
+        person.setAge(17);
+
+        HttpEntity<Person> entity = new HttpEntity<>(person , headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/persons/1"), HttpMethod.PATCH, entity, String.class);
+
+        JSONObject jsonObject = new JSONObject(response.getBody());
+
+        assertTrue(response.getStatusCode() == HttpStatus.OK);
+        assertEquals(jsonObject.get("name"), "TestePatch");
+        assertEquals(jsonObject.get("age"), 17);
+    }
+
+    @Test
+    public void returnNotFoundWhenUserDoesNotExistIT() throws Exception {
+        HttpEntity<Person> entity = new HttpEntity<>(null , headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                createURLWithPort("/persons/1"), HttpMethod.GET, entity, String.class);
+
+        assertTrue(response.getStatusCode() == HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity createPerson(String name, Integer age) {
+        Person person = new Person();
+        person.setName(name);
+        person.setAge(age);
+
+        HttpEntity<Person> entity = new HttpEntity<>(person, headers);
+
+        return restTemplate.exchange(
+                createURLWithPort("/persons"), HttpMethod.POST, entity, String.class);
+
     }
 
     private String createURLWithPort(String uri) {
