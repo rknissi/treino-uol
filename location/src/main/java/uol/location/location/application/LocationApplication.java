07ab4a0d2db080d73bdc7e885d9474d67b16eacc
Rafael Nissi <rafael.nissi@usp.br>
2020-07-26 21:59:08 -0300

    Agora a fila chama o método de criação do Location completo e funciona corretamente

--- location/src/main/java/uol/location/location/application/LocationApplication.java
+++ location/src/main/java/uol/location/location/application/LocationApplication.java
@@ -2,9 +2,12 @@ package uol.location.location.application;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import uol.location.location.gateway.LocationGateway;
 import uol.location.location.objects.Location;
+import uol.location.location.objects.Weather;
 import uol.location.location.repository.objects.LocationRepositoryEntity;
 import uol.location.location.repository.LocationRepository;
+import uol.location.location.repository.objects.WeatherRepositoryEntity;
 
 import java.util.Optional;
 
@@ -13,12 +16,14 @@ public class LocationApplication {
 
 	private final LocationRepository locationRepository;
 	private final MessageSenderApplication messageSenderApplication;
-	private final LocationAsyncApplication locationAsyncApplication;
+	private final LocationGateway locationGateway;
+	private final WeatherApplication weatherApplication;
 
-	public LocationApplication(LocationRepository locationRepository, MessageSenderApplication messageSenderApplication, LocationAsyncApplication locationAsyncApplication) {
+	public LocationApplication(LocationRepository locationRepository, MessageSenderApplication messageSenderApplication, LocationGateway locationGateway, WeatherApplication weatherApplication) {
 		this.locationRepository = locationRepository;
 		this.messageSenderApplication = messageSenderApplication;
-		this.locationAsyncApplication = locationAsyncApplication;
+		this.locationGateway = locationGateway;
+		this.weatherApplication = weatherApplication;
 	}
 
 	public Location create(String ipv4) {
@@ -26,14 +31,30 @@ public class LocationApplication {
 		LocationRepositoryEntity locationRepositoryEntity = new LocationRepositoryEntity();
 		locationRepository.save(locationRepositoryEntity);
 		messageSenderApplication.sendMessage(ipv4, locationRepositoryEntity.getId());
-		//locationAsyncApplication.populateData(ipv4, locationRepositoryEntity);
 		BeanUtils.copyProperties(locationRepositoryEntity, location);
 		return location;
 	}
 
+	public void populateData(String ipv4, LocationRepositoryEntity locationRepositoryEntity) {
+		Long id = locationRepositoryEntity.getId();
+		Location location = locationGateway.getGeographicalLocation(ipv4);
+		BeanUtils.copyProperties(location, locationRepositoryEntity);
+		locationRepositoryEntity.setId(id);
+
+		Weather weather = weatherApplication.getWeatherFromLatLog(location.getLatitude(), location.getLongitude());
+		weather.setId(null);
+		WeatherRepositoryEntity weatherRepositoryEntity = new WeatherRepositoryEntity();
+		BeanUtils.copyProperties(weather, weatherRepositoryEntity);
+		locationRepositoryEntity.setWeather(weatherRepositoryEntity);
+
+		location.setWeather(weather);
+
+		locationRepository.save(locationRepositoryEntity);
+	}
+
 
 	public Location getById(Long id) {
-        Optional<LocationRepositoryEntity> locationEntity = locationRepository.findById(id);
+		Optional<LocationRepositoryEntity> locationEntity = locationRepository.findById(id);
         if (locationEntity.isPresent()) {
         	Location location = new Location();
 			BeanUtils.copyProperties(locationEntity.get(), location);
--- location/src/main/java/uol/location/location/application/MessageReceiverApplication.java
+++ location/src/main/java/uol/location/location/application/MessageReceiverApplication.java
@@ -5,14 +5,21 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import org.springframework.stereotype.Component;
+import org.springframework.util.SerializationUtils;
+import uol.location.location.queue.LocationQueue;
+import uol.location.location.repository.objects.LocationRepositoryEntity;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 @Component
 public class MessageReceiverApplication {
+    private final LocationApplication locationApplication;
     private final static String QUEUE_NAME = "create-weather";
-    public MessageReceiverApplication () throws IOException, TimeoutException {
+
+    public MessageReceiverApplication (LocationApplication locationApplication) throws IOException, TimeoutException {
+        this.locationApplication = locationApplication;
+
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         factory.setPort(5672);
@@ -25,7 +32,11 @@ public class MessageReceiverApplication {
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
+            LocationQueue locationQueue = (LocationQueue) SerializationUtils.deserialize(delivery.getBody());
             System.out.println(" [x] Received '" + message + "'");
+            LocationRepositoryEntity locationRepositoryEntity = new LocationRepositoryEntity();
+            locationRepositoryEntity.setId(locationQueue.getId());
+            locationApplication.populateData(locationQueue.getIp(), locationRepositoryEntity);
         };
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
