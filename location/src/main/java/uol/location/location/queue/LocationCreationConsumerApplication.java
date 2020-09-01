074976e01e359726f6d1fcf6cc47905f665c7f91
Rafael Nissi <rafael.nissi@usp.br>
2020-09-01 11:50:22 -0300

    Criado arquivos do Postman; Agora se der erro na criação do Location, ele fica dando retries

--- location/src/main/java/uol/location/location/queue/LocationCreationConsumerApplication.java
+++ location/src/main/java/uol/location/location/queue/LocationCreationConsumerApplication.java
@@ -2,9 +2,11 @@ package uol.location.location.queue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.ImmediateRequeueAmqpException;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import uol.location.location.application.LocationApplication;
 
 @Component
@@ -20,6 +22,12 @@ public class LocationCreationConsumerApplication {
     public void receive(String value) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         LocationCreationMessage locationCreationMessage = objectMapper.readValue(value, LocationCreationMessage.class);
-        locationApplication.populateData(locationCreationMessage);
+        try {
+            locationApplication.populateData(locationCreationMessage);
+            System.out.println("Testeeeeeeeeee!");
+        } catch (HttpClientErrorException e) {
+            System.out.println("Teste!");
+            throw new ImmediateRequeueAmqpException(value);
+        }
     }
 }
