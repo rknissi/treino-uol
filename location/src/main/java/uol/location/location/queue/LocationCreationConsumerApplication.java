9dc44e0ffcc3292a7dfbf590831a915164e34ea5
Rafael Nissi <rafael.nissi@usp.br>
2020-09-08 21:58:45 -0300

    Criação inicial dos exchanges

--- location/src/main/java/uol/location/location/queue/LocationCreationConsumerApplication.java
+++ location/src/main/java/uol/location/location/queue/LocationCreationConsumerApplication.java
@@ -3,14 +3,14 @@ package uol.location.location.queue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.ImmediateRequeueAmqpException;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import uol.location.location.application.LocationApplication;
 
 @Component
-@RabbitListener(queues = "location-creation")
+//@RabbitListener(queues = "location-creation")
+@RabbitListener(bindings = @QueueBinding(exchange = @Exchange("myExchange"), value = @Queue("location-creation")))
 public class LocationCreationConsumerApplication {
     private final LocationApplication locationApplication;
 
--- location/src/main/java/uol/location/location/queue/LocationDeleteConsumerApplication.java
+++ location/src/main/java/uol/location/location/queue/LocationDeleteConsumerApplication.java
@@ -2,15 +2,15 @@ package uol.location.location.queue;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.ImmediateRequeueAmqpException;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.stereotype.Component;
 import uol.location.location.application.LocationApplication;
 
 import java.io.IOException;
 
 @Component
-@RabbitListener(queues = "location-delete")
+//@RabbitListener(queues = "location-delete")
+@RabbitListener(bindings = @QueueBinding(exchange = @Exchange("myExchange"), value = @Queue("location-delete")))
 public class LocationDeleteConsumerApplication {
     private final LocationApplication locationApplication;
 
